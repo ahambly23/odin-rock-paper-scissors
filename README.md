68ab081da3484f3588eca98c266f196dfa266749
ahambly23 <ashleyhambly4@hotmail.com>
2024-09-15 00:21:44 +0100

    Added transitions on click

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # rock-paper-scissors
 The Odin Project foundations project where I create a game of rock, paper, scissors which is played within the console.
 
-View project here: https://ahambly23.github.io/odin-rock-paper-scissors/
+Add shadows around the choices after each round
+    green for human, red for computer, orange for a tie
+    e.g. 
+    if human === rock && computer === scissors
+        rock.style.textShadow = "etc"
+
+View the live demo here: https://ahambly23.github.io/odin-rock-paper-scissors/
--- main.js
+++ main.js
@@ -22,6 +22,7 @@ let humanScore = 0;
 let resultCont = document.querySelector(".div-cont")
 let resetCont = document.querySelector(".reset")
 let result = document.createElement("p");
+result.classList.add("result");
 let hScore = document.createElement("p");
 let cScore = document.createElement("p");
 
@@ -114,23 +115,59 @@ const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
 
+const resetTextShadow = () => {
+    rock.style.textShadow = "none";
+    paper.style.textShadow = "none";
+    scissors.style.textShadow = "none";
+}
+
 rock.addEventListener("click", () => {
     if (computerScore < 5 && humanScore < 5) {
+        resetTextShadow();
         let computerChoice = getComputerChoice();
         playRound(computerChoice, "rock");
+        if (computerChoice === "rock") {
+            rock.style.textShadow = "6px 6px 3px orange";
+        } else if (computerChoice === "scissors") {
+            rock.style.textShadow = "6px 6px 3px green";
+            scissors.style.textShadow = "6px 6px 3px red";
+        } else if (computerChoice === "paper") {
+            rock.style.textShadow = "6px 6px 3px red";
+            paper.style.textShadow = "6px 6px 3px green";
+        }
     }
 })
 
 paper.addEventListener("click", () => {
     if (computerScore < 5 && humanScore < 5) {
+        resetTextShadow();
         let computerChoice = getComputerChoice();
         playRound(computerChoice, "paper");
+        if (computerChoice === "paper") {
+            paper.style.textShadow = "6px 6px 3px orange";
+        } else if (computerChoice === "scissors") {
+            paper.style.textShadow = "6px 6px 3px red";
+            scissors.style.textShadow = "6px 6px 3px green";
+        } else if (computerChoice === "rock") {
+            rock.style.textShadow = "6px 6px 3px red";
+            paper.style.textShadow = "6px 6px 3px green";
+        }
     }
 })
 
 scissors.addEventListener("click", () => {
     if (computerScore < 5 && humanScore < 5) {
+        resetTextShadow();
         let computerChoice = getComputerChoice();
         playRound(computerChoice, "scissors");
+        if (computerChoice === "scissors") {
+            scissors.style.textShadow = "6px 6px 3px orange";
+        } else if (computerChoice === "rock") {
+            rock.style.textShadow = "6px 6px 3px green";
+            scissors.style.textShadow = "6px 6px 3px red";
+        } else if (computerChoice === "paper") {
+            scissors.style.textShadow = "6px 6px 3px green";
+            paper.style.textShadow = "6px 6px 3px red";
+        }
     }
 })
